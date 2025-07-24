8873b1793b767ea2cc9fb0b120ee4c717d10b994
Manish Ahire <ahiremanishb@gmail.com>
2025-07-24 15:58:44 +0530

    Max Subarray sum using Brute Force

---
+++ Concepts/MaxSubarraySum-BruteForce.playground/Contents.swift
@@ -0,0 +1,26 @@
+// Print max subarray sum using brute force approch
+
+func maxSubArraySum(_ numbers: [Int]) -> Int {
+    
+    var maxSum = 0
+    
+    for start in 0..<numbers.count {
+        for end in (start + 1)...numbers.count {
+            
+            let subarray = Array(numbers[start..<end])
+            var currentSum = 0
+            for number in subarray {
+                currentSum += number
+            }
+            
+            if currentSum > maxSum {
+                maxSum = currentSum
+            }
+        }
+    }
+    return maxSum
+}
+
+let numbers = [1, 2, 3, 4, 5]
+let maxSum = maxSubArraySum(numbers)
+print("Max Sum: \(maxSum)")
